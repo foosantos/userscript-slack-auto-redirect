89d8348ccc8eb33f56882c8735f6ae373cf01f7b
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2022-08-23 14:52:06 -0300

    Remove unnecessary code

--- userscript-slack-auto-redirect.user.js
+++ userscript-slack-auto-redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-redirect for browser Slack users
 // @namespace    https://github.com/felipelousantos/userscript-slack-auto-redirect
-// @version      0.1
+// @version      0.2
 // @description  If you are a Slack browser user, this userscript will allow you to auto-redirect without Slack asking if you want to open on the desktop app.
 // @author       felipelousantos
 // @updateURL    https://github.com/felipelousantos/userscript-slack-auto-redirect/raw/main/userscript-slack-auto-redirect.user.js
@@ -11,10 +11,8 @@
 // ==/UserScript==
 
 (function() {
-    if (document.getElementsByClassName('p-ssb_redirect')[0] =! null){
-        const currentLink = window.location.href;
-        const browserLink = currentLink.replace("archives", "messages");
-        document.getElementsByClassName('p-ssb_redirect')[0].innerHTML = "";
-        location.replace(browserLink);
-    }
+    const currentLink = window.location.href;
+    const browserLink = currentLink.replace("archives", "messages");
+    document.getElementsByClassName('p-ssb_redirect')[0].innerHTML = "";
+    location.replace(browserLink);
 })();
